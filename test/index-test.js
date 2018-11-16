431cc983f8e103f52d497bc0d2d9a4a8da9467ad
Natalie Halawani <hala1224@gmail.com>
2018-11-16 02:52:29 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,8 @@
 describe('index', () => {
-  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
   function triggerKeyDown(which) {
-    const keyboardEvent = document.createEvent("KeyboardEvent")
+    const keyboardEvent = document.createEvent("KeyboardEvent");
 
     keyboardEvent.initKeyboardEvent(
       'keydown',
@@ -14,34 +14,34 @@ describe('index', () => {
       0,
       null,
       null
-    )
+    );
 
-    document.body.dispatchEvent(keyboardEvent)
+    document.body.dispatchEvent(keyboardEvent);
   }
 
   describe('Konami code', () => {
     it('triggers an alert if the right code is entered', () => {
-      init()
+      init();
 
-      window.alert = expect.createSpy()
+      window.alert = expect.createSpy();
 
       for (let i = 0, l = code.length; i < l; i++) {
-        triggerKeyDown(code[i])
+        triggerKeyDown(code[i]);
       }
 
-      expect(window.alert).toHaveBeenCalled()
-    })
+      expect(window.alert).toHaveBeenCalled();
+    });
 
     it('does not trigger an alert if the wrong code is entered', () => {
-      init()
+      init();
 
-      window.alert = expect.createSpy()
+      window.alert = expect.createSpy();
 
       for (let i = 0, l = code.length; i < l; i++) {
-        triggerKeyDown(i)
+        triggerKeyDown(i);
       }
 
-      expect(window.alert).toNotHaveBeenCalled()
-    })
-  })
-})
+      expect(window.alert).toNotHaveBeenCalled();
+    });
+  });
+});
