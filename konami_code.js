c78ffb8c3b5ec7ffa9812e6a20e9b40c563159f7
Natalie Halawani <hala1224@gmail.com>
2018-11-16 01:48:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+let index = 0;
+
+function init(e) {
   // Write your JavaScript code inside the init() function
+  const key = parse.Int(e.detail || e.which);
+  
+  if (key === code[index]) {
+    index++;
+    if (index === code.length) {
+      
+    }
+  }
 
 }
