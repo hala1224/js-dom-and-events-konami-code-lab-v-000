0b892a3aef1944a9ddcd10ecd1b30ee34abaea48
Natalie Halawani <hala1224@gmail.com>
2018-11-16 02:21:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,7 +11,7 @@ function init() {
       index++;
       if(index===code.length){
         window.alert = expect.createSpy('Hooray!');
-        window.alert;
+        // window.alert;
       }
     }
     else{
