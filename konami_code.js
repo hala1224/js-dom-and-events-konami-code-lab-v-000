778389baab7d317e9ec69f9ce5dd12909ed72a91
Natalie Halawani <hala1224@gmail.com>
2018-11-16 01:55:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,18 +2,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-function init(e) {
-  // Write your JavaScript code inside the init() function
-  const key = parseInt(e.detail || e.which);
-  
-  if (key === code[index]) {
-    index++;
-    if (index === code.length) {
-      alert("Hooray!");
-      index=0;
-    } else {
+
+function init() {
+  window.addEventListener("keydown", function(event){
+    const key=parseInt(event.which);
+    console.log(event.which);
+    if(key===code[index]){
+      index++;
+      if(index===code.length){
+        window.alert("Cheat activated, you cheater")
+      }
+    }
+    else{
       index=0;
     }
-  }
-
+  })
 }
+init();
