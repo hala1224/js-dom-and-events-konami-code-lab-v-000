96bd001175a256e95ae2c42fac818bfbc3613029
Natalie Halawani <hala1224@gmail.com>
2018-11-16 02:13:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,15 +2,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-
+function spy() {
+   window.alert("Hooray!");
+}
 function init() {
   window.addEventListener("keydown", function(event){
     const key=parseInt(event.which);
-    console.log(event.which);
+    
     if(key===code[index]){
       index++;
       if(index===code.length){
-        window.alert("Hooray!");
+        spy();
       }
     }
     else{
